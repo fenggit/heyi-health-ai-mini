e210dfaaf659146c6744ea65ef70955f20498d6b
felix <fenggit@163.com>
2026-04-06 19:04:23 +0800

    删除无用代码

--- miniprogram/utils/navigateReport.js
+++ miniprogram/utils/navigateReport.js
@@ -3,7 +3,7 @@
  */
 function navigateToReport() {
   wx.redirectTo({
-    url: '/pages/webview-page/index?title=' + encodeURIComponent('测评报告') + '&url=' + encodeURIComponent('https://www.baidu.com/')
+    url: '/pages/webview-page/index?title=' + encodeURIComponent('测评报告') + '&url=' + encodeURIComponent('https://www.baidu.com')
   })
 }
 
